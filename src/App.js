d59a1ce82d8a58f048ce29e9fd189ebf8ed532bf
natelba <77124289+natelba@users.noreply.github.com>
2021-06-08 15:59:39 -0400

    editing files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
-
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+     <h1> Weather App </h1>
+      <footer>
+        Projected created by <strong>Natalia Guardia</strong> and is {" "}
+        <a href="https://www.github.com/natelba/final-weather-react" target="_blank" rel="noreferrer">
+          Open-Sourced
+        </a>.
+      </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
